bba0e522be52f0b1c0ea9f7560820bf30b5d2047
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-01-11 23:15:10 +0100

    render option

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,33 @@
 // import { useState } from 'react'
-import { PageTitle } from './PageTitle/PageTitle';
+import { useState } from 'react';
+import { Description } from './Description/Description';
+import { Options } from './Options/Options';
+import { Feedback } from './Feedback/Feedback';
 import './App.css';
 
 export const App = () => {
+  const [feedbackTypes] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
   return (
     <>
-      <PageTitle text="Sip Happens Café" />
       <ul>
-        <li></li>
+        <li>
+          <Description text="Sip Happens Café">
+            Please leave your feedback about our service by selecting one of the options below.
+          </Description>
+        </li>
+
+        <li>
+          <Options />
+        </li>
+
+        <li>
+          <Feedback value={feedbackTypes} />
+        </li>
       </ul>
     </>
   );
