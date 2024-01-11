414488355ae96be8ff3762fbc478fb671afe6e5b
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-01-12 00:41:24 +0100

    positive match

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,6 +1,6 @@
 export const Feedback = ({ value: { good, neutral, bad } }) => {
   const totalFeedback = good + neutral + bad;
-
+  const positive = Math.round(((good + neutral) / totalFeedback) * 100);
   return (
     <div>
       {totalFeedback > 0 ? (
@@ -8,6 +8,8 @@ export const Feedback = ({ value: { good, neutral, bad } }) => {
           <p>Good: {good}</p>
           <p>Neutral: {neutral}</p>
           <p>Bad: {bad}</p>
+          <p>Total:{totalFeedback}</p>
+          <p>Positive{positive}</p>
         </>
       ) : (
         <p>No feedback yet.</p>
