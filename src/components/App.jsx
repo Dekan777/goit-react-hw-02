3245385f068c0cee598dea369ac7c8bb887e72d9
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-01-11 22:01:03 +0100

    h1
     Sip Happens Café

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
 // import { useState } from 'react'
-
+import { PageTitle } from './PageTitle/PageTitle';
 import './App.css';
 
 export const App = () => {
   return (
     <>
+      <PageTitle text="Sip Happens Café" />
       <ul>
         <li></li>
       </ul>
