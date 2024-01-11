499094f3d6299e85a3b8264681e0e5f0788b40f3
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-01-12 00:33:29 +0100

    reset

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -2,14 +2,22 @@ export const Options = ({ value, valueSet }) => {
   const handleClick = type => {
     const newFeedbackTypes = { ...value };
     newFeedbackTypes[type] += 1;
+
     valueSet(newFeedbackTypes);
   };
+
+  const handleReset = () => {
+    valueSet({ good: 0, neutral: 0, bad: 0 });
+  };
+
+  const hasFeedback = value.good + value.neutral + value.bad > 0;
 
   return (
     <div>
       <button onClick={() => handleClick('good')}>Good</button>
       <button onClick={() => handleClick('neutral')}>Neutral</button>
       <button onClick={() => handleClick('bad')}>Bad</button>
+      {hasFeedback && <button onClick={handleReset}>Reset</button>}
     </div>
   );
 };
