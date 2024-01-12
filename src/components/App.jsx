cd73f6fd5e98aaf193273033e4379bb139b45a28
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-01-12 01:51:55 +0100

    end

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
 // import { useState } from 'react'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Description } from './Description/Description';
 import { Options } from './Options/Options';
 import { Feedback } from './Feedback/Feedback';
 import './App.css';
 
 export const App = () => {
-  const [feedbackTypes, setFeedbackTypes] = useState({
+  const initialFeedbackTypes = JSON.parse(window.localStorage.getItem('saved-value')) || {
     good: 0,
     neutral: 0,
     bad: 0,
-  });
+  };
+
+  const [feedbackTypes, setFeedbackTypes] = useState(initialFeedbackTypes);
+
+  useEffect(() => {
+    window.localStorage.setItem('saved-value', JSON.stringify(feedbackTypes));
+  }, [feedbackTypes]);
 
   return (
     <>
