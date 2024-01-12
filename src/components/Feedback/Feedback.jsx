7eba6d20bd2c7bef3374741c2fa7ba5d4a5aa4a8
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-01-12 02:05:30 +0100

    end

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,3 +1,4 @@
+import css from './Feedback.module.css';
 export const Feedback = ({ value: { good, neutral, bad } }) => {
   const totalFeedback = good + neutral + bad;
   const positive = Math.round(((good + neutral) / totalFeedback) * 100);
@@ -5,11 +6,11 @@ export const Feedback = ({ value: { good, neutral, bad } }) => {
     <div>
       {totalFeedback > 0 ? (
         <>
-          <p>Good: {good}</p>
-          <p>Neutral: {neutral}</p>
-          <p>Bad: {bad}</p>
-          <p>Total:{totalFeedback}</p>
-          <p>Positive{positive}</p>
+          <p className={css.text}>Good: {good}</p>
+          <p className={css.text}>Neutral: {neutral}</p>
+          <p className={css.text}>Bad: {bad}</p>
+          <p className={css.text}>Total:{totalFeedback}</p>
+          <p className={css.text}>Positive{positive}</p>
         </>
       ) : (
         <p>No feedback yet.</p>
